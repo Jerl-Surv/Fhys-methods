f1f98f2e92d0106e8de840f129f907a6f0bbeb8a
Jerl-Surv <duflot.julia@gmail.com>
2016-03-28 18:18:23 +0300

    для вэжх
    Оно работает

--- urie.py
+++ urie.py
@@ -1,26 +1,20 @@
 H = [960, 860, 920, 1120]
 F = [16, 13, 6, 3]
 
-Hp = []
-N = 0 
-
 for A in range(0, 340, 10):
-	for B in range(600, 4600, 100):
-		for C in range(26, 54, 1):
-			
-#			for i in range(1, 3, 1):
-#			    Ft = F[i]
-#			    Ht = H[i]
-#			    Hpt = A + B/Ft + C*Ft
-#				if (Ht-3) < Hpt < (Ht+3):
-#					N = N+1
-
-			for i in range(3):
-				Hp[i] = A + B / F[i] + C * F[i]
-				if (H[i] - 3)<Hp[i]<(H[i] + 3):
-					N = N+1	
-			
-			if N > 2:
-				print ('It seems ok: ', 'A = ', A, 'B = ', B, 'C = ', C, Hp)
-			Hp = []
-			N = 0
+    Hp = []
+    N = 0
+    for B in range(600, 4600, 100):
+        for C in range(26, 54, 1):
+            for i in range(4):
+                Hp.append(int(A + B / F[i] + C * F[i]))
+                print(Hp)
+                if (H[i] - 30)<Hp[i]<(H[i] + 30):
+                    N = N+1				
+            if N > 2:
+                print ('It seems ok: ', 'A = ', A, 'B = ', B, 'C = ', C, Hp)
+                Hp = []
+                N = 0
+            else:
+                Hp = []
+                N = 0
