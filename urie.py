0eb8c1673cf0d32eb0c8ffe20a809f16fef7d614
Jerl-Surv <duflot.julia@gmail.com>
2016-03-28 19:20:22 +0300

    Для вэжх
    Исправлены интервалы

--- urie.py
+++ urie.py
@@ -1,17 +1,17 @@
-H = [960, 860, 920, 1120]
+H = [957, 862, 917, 1122]
 F = [16, 13, 6, 3]
 
-for A in range(0, 340, 10):
+for A in range(0, 600, 10):
     Hp = []
     N = 0
-    for B in range(600, 4600, 100):
-        for C in range(26, 54, 1):
+    for B in range(600, 5600, 100):
+        for C in range(16, 54, 1):
             for i in range(4):
                 Hp.append(int(A + B / F[i] + C * F[i]))
-                print(Hp)
+                #print(Hp)
                 if (H[i] - 30)<Hp[i]<(H[i] + 30):
                     N = N+1				
-            if N > 2:
+            if N > 3:
                 print ('It seems ok: ', 'A = ', A, 'B = ', B, 'C = ', C, Hp)
                 Hp = []
                 N = 0
